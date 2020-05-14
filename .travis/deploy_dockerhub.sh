673b54b8dc1fdf18d640d7788dec493224127cee
Atibhi Agrawal <atibhi.a@gmail.com>
2020-05-14 21:24:29 +0530

    cleanup deploy_dockerhub.sh script

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,10 +1,8 @@
-docker login --username $DOCKER_USER --password $DOCKER_PASS
 if [ "$TRAVIS_BRANCH" = "master" ]; then
 TAG="latest"
 else
 TAG="$TRAVIS_BRANCH"
 fi
 docker build -f Dockerfile -t "asquare14/whats-slot-iiitb":$TAG .
-docker tag "asquare14/whats-slot-iiitb" $DOCKER_REPO
 docker login --username=$DOCKER_USER --password=$DOCKER_PASS
 docker push asquare14/whats-slot-iiitb:$TAG
