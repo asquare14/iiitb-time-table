bce30f10e48885097351757ff611dba88c33ec64
Atibhi Agrawal <atibhi.a@gmail.com>
2020-05-16 22:09:37 +0530

    add imports file

--- app.py
+++ app.py
@@ -1,19 +1,5 @@
-from flask import Flask
-import json, os, re
-import functools
-from flask import (
-    Blueprint, flash, g, redirect, render_template, request, session, url_for
-)
-from search import searchData
-import os
-import itertools
-import requests
-import json
-import re
-import sys
-import collections
+from imports import *
 from prof import *
-
 
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
--- prof.py
+++ prof.py
@@ -1,18 +1,4 @@
-from flask import Flask
-import json, os, re
-import functools
-from flask import (
-    Blueprint, flash, g, redirect, render_template, request, session, url_for
-)
-from search import searchData
-import os
-import itertools
-import requests
-import json
-import re
-import sys
-import collections
-import app
+from imports import *
 
 script_dir = os.path.dirname(__file__)
 
