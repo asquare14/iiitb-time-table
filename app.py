f78169b93c0b7f4c15d525a0ddc47643f210f542
Atibhi Agrawal <atibhi.a@gmail.com>
2020-05-16 22:04:05 +0530

    Remove Spell Check

--- app.py
+++ app.py
@@ -67,67 +67,6 @@ class CaseInsensitiveDict(dict):
         self.proxy[k.lower()] = k
 
 
-class SpellingCorrector():
-    """
-        Spelling Corrector in Python 3; see http://norvig.com/spell-correct.html
-        Copyright (c) 2007-2016 Peter Norvig
-        MIT license: www.opensource.org/licenses/mit-license.php
-    """
-
-
-    word_list = []
-    WORDS = collections.Counter([])
-
-
-    def __init__(self, words):
-        self.word_list = [t.lower() for t in words]
-        self.WORDS = collections.Counter(self.word_list)
-
-
-    def P(self, word, N=sum(WORDS.values())):
-        "Probability of `word`."
-        if N == 0:
-            return 0
-        return self.WORDS[word] / N
-
-
-    def correction(self, word):
-        "Most probable spelling correction for word."
-        return max(self.candidates(word), key=self.P)
-
-
-    def candidates(self, word):
-        "Generate possible spelling corrections for word."
-        return (self.known([word]) or self.known(self.edits1(word)) or self.known(self.edits2(word)) or [word])
-
-
-    def known(self, words):
-        "The subset of `words` that appear in the dictionary of WORDS."
-        return set(w for w in words if w in self.WORDS)
-
-
-    def edits1(self, word):
-        "All edits that are one edit away from `word`."
-
-        letters    = 'abcdefghijklmnopqrstuvwxyz'
-        splits     = [(word[:i], word[i:])    for i in range(len(word) + 1)]
-        deletes    = [L + R[1:]               for L, R in splits if R]
-        transposes = [L + R[1] + R[0] + R[2:] for L, R in splits if len(R)>1]
-        replaces   = [L + c + R[1:]           for L, R in splits if R for c in letters]
-        inserts    = [L + c + R               for L, R in splits for c in letters]
-
-        return set(deletes + transposes + replaces + inserts)
-
-
-    def edits2(self, word):
-        "All edits that are two edits away from `word`."
-
-        return (e2 for e1 in self.edits1(word) for e2 in self.edits1(e1))
-
-
-
-
-
 with open(os.path.join(script_dir, 'data/data.json'), 'r') as f:
     profs_dict = CaseInsensitiveDict(json.load(f))
 
@@ -216,8 +155,6 @@ with open(os.path.join(script_dir, 'data/data.json')) as f:
 def fetch_results(prof):
     tb = [[['Monday']], [['Tuesday']], [['Wednesday']], [['Thursday']], [['Friday']]]
     times = ['', '8 AM', '9 AM', '10 AM', '11 AM', '12 PM', '2 PM', '3 PM', '4 PM', '5 PM']
-
-    prof = correct_spelling(prof)
     slot_data = get_times(prof)
     dept = get_attr(prof, 'dept')
     website = get_attr(prof, 'website')
@@ -240,13 +177,6 @@ def fetch_results(prof):
             tb[int(item[0])][int(item[1])+1].append(venue)
 
     return [tb, times, dept, website, prof.title()]
-
-def correct_spelling(prof_name):
-	prof_names = profs_dict.keys()
-	if prof_name not in prof_names:
-		corrector = SpellingCorrector(prof_names)
-		return corrector.correction(prof_name)
-	return prof_name
 
 @app.route('/professor', methods=['POST'])
 def result():
