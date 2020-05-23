ceefaa567c068cc60f4534d529702352dd249ee7
Atibhi Agrawal <atibhi.a@gmail.com>
2020-05-23 19:45:14 +0530

    Fix Travis

--- .travis/deploy_heroku.sh
+++ .travis/deploy_heroku.sh
@@ -2,5 +2,5 @@ wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 heroku plugins:install @heroku-cli/plugin-container-registry
 docker login --username _ --password=$HEROKU_API_KEY registry.heroku.com
 heroku container:login
-heroku container:push web --app $HEROKU_APP_NAME_TEST
-heroku container:release web --app $HEROKU_APP_NAME_TEST
+heroku container:push web --app $HEROKU_APP_NAME
+heroku container:release web --app $HEROKU_APP_NAME
