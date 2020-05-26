452d042d16810f0709f5eb6fe8d5e11d902ae2ef
Atibhi Agrawal <atibhi.a@gmail.com>
2020-05-26 22:26:54 +0530

    Clash try (#54)
    * remove clash by deselcting courses
    
    * clash unselect
    
    * Data added
    
    * duplicate prof in json, bug
    
    * clash
    
    Co-authored-by: Shreya <Shreya.Singh@iiitb.org>
    Co-authored-by: Shreya Singh <33528455+singh-shreya321@users.noreply.github.com>

--- static/mainScript.js
+++ static/mainScript.js
@@ -70,7 +70,6 @@ function getTimeTable(){
             }
         }
     }
-    console.log(timetable)
     return timetable
 }
 
@@ -94,66 +93,131 @@ function addToCalendar(){
 
 };        
 
-function sdCallback(data, id, course) {
-    console.log("yo")
-    if (typeof data['Name'] !== "undefined") {
-        if (id === undefined) {
-            var details = "";
-            var x = 0;
-            details += "<b>Name: </b>" + data['Name'] + " <br>";
-            courseData = data['Data'];
-            for (var key in courseData) {
-                if(key != 'Slot'){
-                    console.log(key)
-                    details += "<b>" + key + ": </b>" + courseData[key] + " <br>";
+function sdCallback(data, id, course, clear_var) {
+    if(clear_var == 0){
+        if (typeof data['Name'] !== "undefined") {
+            if (id === undefined) {
+                var details = "";
+                var x = 0;
+                details += "<b>Name: </b>" + data['Name'] + " <br>";
+                courseData = data['Data'];
+                for (var key in courseData) {
+                    if(key != 'Slot'){
+                        details += "<b>" + key + ": </b>" + courseData[key] + " <br>";
+                    }
                 }
-            }
-            var old_name = []
-            for (var slot in courseData['Slot']) {
-                $('#' + courseData['Slot'][slot]).addClass('border border-danger');
-                if($('#' + courseData['Slot'][slot]).hasClass('table-danger')){
-                    $('#' + courseData['Slot'][slot]).removeClass('table-danger');
-                    $('#' + courseData['Slot'][slot]).addClass('table-clash');
-                    curr_name = $('#' + courseData['Slot'][slot]).html()
-                    if(curr_name.length == 0)
-                        curr_name = "your occupied slot";
-                    if(old_name.includes(curr_name) == 0)
-                        old_name.push(curr_name)
-                    x = 1;
-                }
-            }
-            if(x == 0){
-                $('#details-div').html("");
-                $('#details-div').html(details);
+                var old_name = []
                 for (var slot in courseData['Slot']) {
-                    $('#' + courseData['Slot'][slot]).addClass('border border-danger');
-                    $('#' + courseData['Slot'][slot]).addClass('table-danger');
-                    $('#' + courseData['Slot'][slot]).html(data['Name'].split(':')[0])
+                    if($('#' + courseData['Slot'][slot]).hasClass('table-danger') || $('#' + courseData['Slot'][slot]).hasClass('table-clash')){
+                        if($('#' + courseData['Slot'][slot]).hasClass('table-danger')){
+                            $('#' + courseData['Slot'][slot]).removeClass('table-danger');
+                            $('#' + courseData['Slot'][slot]).addClass('table-clash');
+                        }
+                        var temp_list = ($('#' + '9' + courseData['Slot'][slot]).html().split(":")); 
+                        var cnt = parseInt(temp_list[0]);
+                        cnt = cnt + 1
+                        temp_list[0] = cnt
+                        temp_list.push(data['Name'].split(":")[0])
+                        console.log(cnt)
+                        $('#9' + courseData['Slot'][slot]).html(temp_list.join(":"))
+                        curr_name = $('#' + courseData['Slot'][slot]).html()
+                        if(curr_name.length == 0)
+                            curr_name = "your occupied slot";
+                        if(old_name.includes(curr_name) == 0)
+                            old_name.push(curr_name)
+                        x = 1;
+                    }
+                }
+                if(x == 0){
+                    $('#details-div').html("");
+                    $('#details-div').html(details);
+                    for (var slot in courseData['Slot']) {
+                        $('#' + courseData['Slot'][slot]).addClass('border border-danger');
+                        $('#' + courseData['Slot'][slot]).addClass('table-danger');
+                        $('#' + courseData['Slot'][slot]).html(data['Name'].split(':')[0])
+                        $('#' + '9' + courseData['Slot'][slot]).html("1" + ":" + data['Name'].split(':')[0])
+                    }
+                }
+                else{
+                     for (var slot in courseData['Slot']) {
+                    if($('#' + courseData['Slot'][slot]).hasClass('table-danger') == 0 && $('#' + courseData['Slot'][slot]).hasClass('table-clash') == 0){
+                        $('#' + courseData['Slot'][slot]).addClass('border border-danger');
+                        $('#' + courseData['Slot'][slot]).addClass('table-danger');
+                        $('#' + courseData['Slot'][slot]).html(data['Name'].split(':')[0])
+                        $('#' + '9' + courseData['Slot'][slot]).html("1" + ":" + data['Name'].split(':')[0])
+                    }
+                }
+                    $('#details-div').html("");
+                    details = "<b> Warning!! </b><br> Course " + data['Name'] + " clashes with "
+                    var i = 0
+                    for(i = 0; i < old_name.length; i++){
+                        if(i > 0 && i < old_name.length - 1)
+                            details += ", ";
+                        else if(i >0 && i == old_name.length - 1)
+                            details += "and ";
+                        details += old_name[i]
+                    }
+                    details += ".<br>";
+                    details += "Unselect one of the clashing courses to continue."
+                    $('#details-div').html(details);
                 }
             }
-            else{
+            else {
                 $('#details-div').html("");
-                details = "<b> Warning!! </b><br> Course " + data['Name'] + " clashes with "
-                var i = 0
-                for(i = 0; i < old_name.length; i++){
-                    if(i > 0 && i < old_name.length - 1)
-                        details += ", ";
-                    else if(i >0 && i == old_name.length - 1)
-                        details += "and ";
-                    details += old_name[i]
-                }
-                details += ".";
-                $('#details-div').html(details);
+                parentList = document.getElementById(id);
+                var item = document.createElement('li');
+                item.className = "course";
+                item.setAttribute("onclick", "searchData(this)");
+                item.innerHTML = course;
+                parentList.appendChild(item);
             }
         }
-        else {
-            $('#details-div').html("");
-            parentList = document.getElementById(id);
-            var item = document.createElement('li');
-            item.className = "course";
-            item.setAttribute("onclick", "searchData(this)");
-            item.innerHTML = course;
-            parentList.appendChild(item);
+    }
+    else{
+        if (typeof data['Name'] !== "undefined") {
+            if (id === undefined) {
+                var details = "";
+                courseData = data['Data'];
+                for (var slot in courseData['Slot']) {
+                    if($('#' + courseData['Slot'][slot]).hasClass('table-danger')){
+                        if((data['Name'].split(':')[0].toString().localeCompare($('#' + courseData['Slot'][slot]).html().toString())) == 0) {
+                            $('#' + courseData['Slot'][slot]).removeClass('table-danger');
+                            $('#' + courseData['Slot'][slot]).removeClass('border border-danger');                        
+                            $('#' + courseData['Slot'][slot]).html("")
+                            $('#9' + courseData['Slot'][slot]).html(":")
+                        }
+                    }
+                    else if($('#' + courseData['Slot'][slot]).hasClass('table-clash')){
+                        var temp_list = ($('#' + '9' + courseData['Slot'][slot]).html().split(":")); 
+                        var cnt = parseInt(temp_list[0]);
+                        cnt = cnt - 1;
+                        console.log(cnt)
+                        var index = temp_list.indexOf(data['Name'].split(":")[0])
+                        console.log(temp_list)
+                        if(index > 0){
+                            temp_list.splice(index, 1)
+                        }
+                        console.log(temp_list)
+                        temp_list[0] = cnt
+                        if(cnt == 1){
+                            $('#' + courseData['Slot'][slot]).removeClass('table-clash');
+                            $('#' + courseData['Slot'][slot]).addClass('table-danger');                       
+                        }
+                        $('#' + courseData['Slot'][slot]).html(temp_list[1])
+                        $('#9' + courseData['Slot'][slot]).html(temp_list.join(":"))
+                    }
+                }
+                $('#details-div').html("");   
+            }
+            else {
+                $('#details-div').html("");
+                parentList = document.getElementById(id);
+                var item = document.createElement('li');
+                item.className = "course";
+                item.setAttribute("onclick", "searchData(this)");
+                item.innerHTML = course;
+                parentList.appendChild(item);
+            }
         }
     }
 }
@@ -164,13 +228,17 @@ function clearSelected() {
 
 
 function searchData(q = $("#search-bar").val(), id = undefined) {
+    clear_var = 0
     if(typeof q.className !== "undefined" && q.className.length == 13){
-        return;
+        clear_var = 1;
     }
     var searchString = "";
     if (typeof q !== "string") {
         searchString = q.innerHTML; // Get list item's name
-        q.className += ' active';
+        if(clear_var == 0)
+            q.className += ' active';
+        else
+            q.className = 'course'
     }
     else {
         searchString = q;
@@ -180,7 +248,7 @@ function searchData(q = $("#search-bar").val(), id = undefined) {
             "query": searchString
         },
         function (data) {
-            sdCallback(data, id, searchString);
+            sdCallback(data, id, searchString, clear_var);
         },
         "json"
     );
@@ -257,13 +325,18 @@ setTimeout(
 
 function toggle(el) {
     element = $(el);
+    element_id = element.attr('id')
+    if(element.hasClass('table-clash')){
+        return;
+    }
     if (element.hasClass('table-danger')){
         if(element.html().length == 0){
             element.removeClass('table-danger');
-            element.removeClass('border-border-danger');
+            element.removeClass('border border-danger');
             details = "You clearerd an occupied slot";
             $('#details-div').html("");
             $('#details-div').html(details);
+            $('#9' + element_id).html(":")
         }
     }
     else{
@@ -271,6 +344,7 @@ function toggle(el) {
         details = "You marked a slot as occupied";
         $('#details-div').html("");
         $('#details-div').html(details);
+        $('#9' + element_id).html("1:")
     }
 }
 
